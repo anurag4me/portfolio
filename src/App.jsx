459569aab12bcea6db04bb4e456dd4f6cf7b04cc
anurag4me <anuragmishra5033@gmail.com>
2025-06-14 20:45:42 +0530

    Build the Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 import LoadingScreen from "./components/LoadingScreen";
+import { Navbar } from "./components/Navbar";
 
 const App = () => {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  
   return (
-    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}</>
+    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      </div>
+    </>
   );
 };
 
