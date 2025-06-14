517240bd5e999a9e2c6a01cdc69c6775d45c8347
anurag4me <anuragmishra5033@gmail.com>
2025-06-14 20:57:50 +0530

    Designed Home Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { useState } from "react";
 import LoadingScreen from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
+import { Home } from "./components/sections/Home";
 
 const App = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
-  
+
   return (
     <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
       <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
       </div>
     </>
   );
