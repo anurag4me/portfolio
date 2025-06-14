ad1d11700a99b008a503a7b9b06568f06da7cb0f
anurag4me <anuragmishra5033@gmail.com>
2025-06-14 21:24:47 +0530

    Designed About Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LoadingScreen from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
+import { About } from "./components/sections/About.jsx";
 
 const App = () => {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -14,6 +15,7 @@ const App = () => {
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
+        <About />
       </div>
     </>
   );
