1e4fbc1a8052c22c056ca0f2dbbe98e0ef54b094
anurag4me <anuragmishra5033@gmail.com>
2025-06-23 14:27:28 +0530

    New Resume Updated

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -99,7 +99,7 @@ export const About = () => {
 
           <center>
             <a
-              href="/portfolio/Anurag Mishra CV Resume.pdf"
+              href="/portfolio/Anurag Mishra Resume.pdf"
               download={true}
               className="block max-w-48 mt-6 bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
             >
